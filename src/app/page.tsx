641dcbad578adddfab59d073f4a857ccd96bc68e
leopy25 <umudumster@gmail.com>
2025-09-01 09:11:00 +0300

    Ana sayfaya 5 buton eklendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,22 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="container min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-center text-3xl font-bold my-8">Arşiv Uygulaması</h1>
-      <div className="flex w-full px-8 justify-between items-center">
-        <Link href="/r-codes" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105">
-          R-kod Arşivi
+      <h1 className="text-center text-3xl font-bold my-8 text-blue-600">Arşiv Uygulaması</h1>
+      <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4">
+        <Link href="/r-codes" className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-4 px-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 w-full sm:w-auto text-center">
+          R-kodlar
         </Link>
-        <Link href="/f-codes" className="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105">
-          F-kod Arşivi
+        <Link href="/f-codes" className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-4 px-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 w-full sm:w-auto text-center">
+          F-kodlar
+        </Link>
+        <Link href="/keywords" className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-4 px-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 w-full sm:w-auto text-center">
+          Anahtar Kelimeler
+        </Link>
+        <Link href="/dashboard" className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-4 px-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 w-full sm:w-auto text-center">
+          Pano
+        </Link>
+        <Link href="/other" className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-4 px-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 w-full sm:w-auto text-center">
+          Diğer
         </Link>
       </div>
     </div>
