33d983a8e2c3f9a8dc4ac6d7aa371d941268a453
leopy25 <umudumster@gmail.com>
2025-09-01 09:20:04 +0300

    Ana sayfa butonları arasına 5cm boşluk eklendi.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-center text-3xl font-bold my-8 text-blue-600">Arşiv Uygulaması</h1>
-      <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-16">
+      <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-20">
         <Link href="/r-codes" className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-4 px-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 w-full sm:w-auto text-center">
           R-kodlar
         </Link>
